3f90d5857fbdf47815031e5521ac2951aff88076
Milton Jonathan <milton.jonathan@cartesi.io>
2020-12-14 15:45:04 -0300

    webapp review to work with yarn - Removing npm's package-lock.json - Removing `dist` generated artifacts

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,6 +1,4 @@
 var path = require('path');
-var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
-var phaser = path.join(pathToPhaser, 'dist/phaser-arcade-physics.min');
 
 module.exports = {
 	entry: './src/app.ts',
@@ -11,8 +9,7 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			{ test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
-			{ test: /phaser\.js$/, loader: 'expose-loader?Phaser' }
+			{ test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' }
 		]
 	},
 	devServer: {
@@ -23,9 +20,6 @@ module.exports = {
 		open: true
 	},
 	resolve: {
-		extensions: ['.ts', '.js'],
-		alias: {
-			phaser: phaser
-		  }
+		extensions: ['.ts', '.js']
 	}
 };
