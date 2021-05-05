4354494bc568c3dff4d321c12c2eac127e1a9572
Milton Jonathan <milton.jonathan@cartesi.io>
2021-05-05 19:57:23 -0300

    Formatiing

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -17,8 +17,7 @@ const config: HardhatUserConfig = {
     external: {
         contracts: [
             {
-                artifacts:
-                    "../node_modules/@cartesi/descartes-sdk/export/artifacts",
+                artifacts: "../node_modules/@cartesi/descartes-sdk/export/artifacts",
                 deploy: "../node_modules/@cartesi/descartes-sdk/dist/deploy",
             },
         ],
@@ -46,37 +45,26 @@ const config: HardhatUserConfig = {
 // TASKS
 
 // start-game task
-task("start-game", "Starts a TurnBasedGame instance").setAction(
-    async ({}, hre) => {
-        const { ethers } = hre;
-        const game = await ethers.getContract("TurnBasedGame");
+task("start-game", "Starts a TurnBasedGame instance").setAction(async ({}, hre) => {
+    const { ethers } = hre;
+    const game = await ethers.getContract("TurnBasedGame");
 
-        const { alice, bob } = await hre.getNamedAccounts();
+    const { alice, bob } = await hre.getNamedAccounts();
 
-        const gameTemplateHash =
-            "0x88040f919276854d14efb58967e5c0cb2fa637ae58539a1c71c7b98b4f959baa";
-        const gameMetadata = "0x";
-        const players = [alice, bob];
-        const playerfunds = [100, 100];
-        const playerinfos = [
-            ethers.utils.hexlify(ethers.utils.toUtf8Bytes("Alice")),
-            ethers.utils.hexlify(ethers.utils.toUtf8Bytes("Bob")),
-        ];
+    const gameTemplateHash = "0x88040f919276854d14efb58967e5c0cb2fa637ae58539a1c71c7b98b4f959baa";
+    const gameMetadata = "0x";
+    const players = [alice, bob];
+    const playerfunds = [100, 100];
+    const playerinfos = [
+        ethers.utils.hexlify(ethers.utils.toUtf8Bytes("Alice")),
+        ethers.utils.hexlify(ethers.utils.toUtf8Bytes("Bob")),
+    ];
 
-        const tx = await game.startGame(
-            gameTemplateHash,
-            gameMetadata,
-            players,
-            playerfunds,
-            playerinfos
-        );
-        const gameReadyEvent = (await tx.wait()).events[0];
-        const index = gameReadyEvent.args._index;
-        console.log(
-            `Game started with index '${index}' (tx: ${tx.hash} ; blocknumber: ${tx.blockNumber})\n`
-        );
-    }
-);
+    const tx = await game.startGame(gameTemplateHash, gameMetadata, players, playerfunds, playerinfos);
+    const gameReadyEvent = (await tx.wait()).events[0];
+    const index = gameReadyEvent.args._index;
+    console.log(`Game started with index '${index}' (tx: ${tx.hash} ; blocknumber: ${tx.blockNumber})\n`);
+});
 
 // join-game task
 task("join-game", "Registers player in the lobby in order to join a game")
@@ -87,107 +75,50 @@ task("join-game", "Registers player in the lobby in order to join a game")
         types.string
     )
     .addOptionalParam("metadata", "Metadata of the game", "0x", types.string)
-    .addOptionalParam(
-        "numplayers",
-        "Number of players in the game",
-        2,
-        types.int
-    )
-    .addOptionalParam(
-        "minfunds",
-        "Minimum amount that needs to be staked in order to join the game",
-        10,
-        types.int
-    )
+    .addOptionalParam("numplayers", "Number of players in the game", 2, types.int)
+    .addOptionalParam("minfunds", "Minimum amount that needs to be staked in order to join the game", 10, types.int)
     .addOptionalParam("player", "Name of the account joining the game", "alice")
-    .addOptionalParam(
-        "playerfunds",
-        "the amount being staked by the player joining the game",
-        100,
-        types.int
-    )
-    .addOptionalParam(
-        "playerinfo",
-        "additional information for the player joining the game",
-        "0x",
-        types.string
-    )
-    .setAction(
-        async (
-            {
-                hash,
-                metadata,
-                numplayers,
-                minfunds,
-                player,
-                playerfunds,
-                playerinfo,
-            },
-            hre
-        ) => {
-            const { ethers } = hre;
+    .addOptionalParam("playerfunds", "the amount being staked by the player joining the game", 100, types.int)
+    .addOptionalParam("playerinfo", "additional information for the player joining the game", "0x", types.string)
+    .setAction(async ({ hash, metadata, numplayers, minfunds, player, playerfunds, playerinfo }, hre) => {
+        const { ethers } = hre;
 
-            // retrieves account from configured named accounts, according to player's name
-            const playerAccount = (await hre.getNamedAccounts())[player];
+        // retrieves account from configured named accounts, according to player's name
+        const playerAccount = (await hre.getNamedAccounts())[player];
 
-            // retrieves game and lobby contracts with signer configured for the specified account
-            // - this means that any transaction submitted will be on behalf of that specified account
-            const game = await ethers.getContract(
-                "TurnBasedGame",
-                playerAccount
-            );
-            const lobby = await ethers.getContract(
-                "TurnBasedGameLobby",
-                playerAccount
-            );
+        // retrieves game and lobby contracts with signer configured for the specified account
+        // - this means that any transaction submitted will be on behalf of that specified account
+        const game = await ethers.getContract("TurnBasedGame", playerAccount);
+        const lobby = await ethers.getContract("TurnBasedGameLobby", playerAccount);
 
-            // submits transaction to the lobby contract to join a game
-            let tx = await lobby.joinGame(
-                hash,
-                metadata,
-                numplayers,
-                minfunds,
-                playerfunds,
-                playerinfo
-            );
+        // submits transaction to the lobby contract to join a game
+        let tx = await lobby.joinGame(hash, metadata, numplayers, minfunds, playerfunds, playerinfo);
 
-            // retrieves transaction's emitted events to report outcome
-            const events = (await tx.wait()).events;
+        // retrieves transaction's emitted events to report outcome
+        const events = (await tx.wait()).events;
 
-            if (events && events.length) {
-                // a GameReady event is emitted by TurnBasedGame if a game starts
-                // - parse event using TurnBasedGame's contract interface
-                const gameReadyEvent = game.interface.parseLog(events[0]);
-                const index = gameReadyEvent.args._index;
-                console.log(
-                    `Game started with index '${index}' (tx: ${tx.hash} ; blocknumber: ${tx.blockNumber})\n`
-                );
-            } else {
-                // no event emitted: show current queue to start a game
-                const queue = await lobby.getQueue(
-                    hash,
-                    metadata,
-                    numplayers,
-                    minfunds
-                );
-                console.log("Player enqueued. Current queue is:");
-                if (queue && queue.length) {
-                    for (let i = 0; i < queue.length; i++) {
-                        player = queue[i];
-                        console.log(`- player ${i}`);
-                        console.log(`  - address: ${player.addr}`);
-                        console.log(`  - funds: ${player.funds}`);
-                        console.log(
-                            `  - info: ${
-                                player.info
-                            } ('${ethers.utils.toUtf8String(player.info)}')`
-                        );
-                    }
+        if (events && events.length) {
+            // a GameReady event is emitted by TurnBasedGame if a game starts
+            // - parse event using TurnBasedGame's contract interface
+            const gameReadyEvent = game.interface.parseLog(events[0]);
+            const index = gameReadyEvent.args._index;
+            console.log(`Game started with index '${index}' (tx: ${tx.hash} ; blocknumber: ${tx.blockNumber})\n`);
+        } else {
+            // no event emitted: show current queue to start a game
+            const queue = await lobby.getQueue(hash, metadata, numplayers, minfunds);
+            console.log("Player enqueued. Current queue is:");
+            if (queue && queue.length) {
+                for (let i = 0; i < queue.length; i++) {
+                    player = queue[i];
+                    console.log(`- player ${i}`);
+                    console.log(`  - address: ${player.addr}`);
+                    console.log(`  - funds: ${player.funds}`);
+                    console.log(`  - info: ${player.info} ('${ethers.utils.toUtf8String(player.info)}')`);
                 }
-                console.log("");
             }
+            console.log("");
         }
-    );
+    });
 
 // get-context task
 task("get-context", "Retrieves a TurnBasedGame context given its index")
@@ -218,9 +149,7 @@ task("get-context", "Retrieves a TurnBasedGame context given its index")
             for (let i = 0; i < turns.length; i++) {
                 const turn = turns[i];
                 let data = undefined;
-                const filter = logger.filters.MerkleRootCalculatedFromData(
-                    turn.dataLogIndex
-                );
+                const filter = logger.filters.MerkleRootCalculatedFromData(turn.dataLogIndex);
                 const logEvents = await logger.queryFilter(filter);
                 if (logEvents && logEvents.length) {
                     data = logEvents[0].args._data;
