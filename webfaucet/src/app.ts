e519c325d42065c9c58ad1e5e1056e6654119038
Milton Jonathan <milton.jonathan@cartesi.io>
2021-06-17 00:36:44 -0300

    Adding webfaucet module - Represents a web app with a frontend for the Poker Token Faucet

---
+++ webfaucet/src/app.ts
@@ -0,0 +1,63 @@
+import { ethers } from "ethers";
+import PokerToken from "./abis/PokerToken.json";
+import PokerTokenFaucet from "./abis/PokerTokenFaucet.json";
+
+declare let window: any;
+
+class PokerFaucet {
+    public static readonly CHAINS = {
+        "0x13881": "Matic Testnet",
+        "0x7a69": "Local Network",
+        "0x539": "Local Network",
+    };
+
+    async init() {
+        if (!window.ethereum) {
+            console.error("Cannot connect to window.ethereum. Is Metamask or a similar plugin installed?");
+            return;
+        }
+        // sets up hooks to update web3 when accounts or chain change
+        window.ethereum.on("accountsChanged", () => {
+            location.reload();
+        });
+        window.ethereum.on("chainChanged", () => {
+            location.reload();
+        });
+
+        // connects to ethereum account
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        // retrieves provider + signer
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+        const network = PokerFaucet.CHAINS[window.ethereum.chainId]
+            ? PokerFaucet.CHAINS[window.ethereum.chainId]
+            : "Unsupported Network";
+
+        const tokenContract = new ethers.Contract(PokerToken.address, PokerToken.abi, signer);
+        document.getElementById("network").innerHTML = network;
+        document.getElementById("address").innerHTML = address;
+        document.getElementById("balance").innerHTML = await tokenContract.balanceOf(address);
+
+        const button = document.getElementById("requestButton");
+        button.addEventListener("click", this.request.bind(this));
+    }
+
+    async request() {
+        // retrieves provider + signer (e.g., from metamask)
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
+
+        const faucetContract = new ethers.Contract(PokerTokenFaucet.address, PokerTokenFaucet.abi, signer);
+
+        await faucetContract.requestTokens();
+        // await tokenContract.mint(address, amount);
+
+        alert(`Requested 1000 POKER tokens for ${address}`);
+    }
+}
+
+var faucet = new PokerFaucet();
+faucet.init();
---
+++ webfaucet/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+	entry: './src/app.ts',
+	devtool: 'source-map',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{ test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' }
+		]
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, './'),
+		publicPath: '/dist/',
+		host: 'localhost',
+		port: 3000,
+		open: true
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	}
+};
