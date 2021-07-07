55493e958d62d40c4dcdde5884ba17be5f185e73
Cláudio Silva <claudio.silva@cartesi.io>
2021-07-07 17:52:34 -0300

    Testing two players joining (#177) - Lobbyweb3 service must be refactored to allow define who is calling the smart contract (msg.sender) - Currently all calls are being from the same caller.

--- webapp/test/services/web3/LobbyWeb3.ts
+++ webapp/test/services/web3/LobbyWeb3.ts
@@ -12,19 +12,31 @@ describe('LobbyWeb3', () => {
 
     beforeEach(async () => {
         const { provider } = ServiceConfig.getProviderConfiguration();
-        const signer = provider.getSigner();
-        const signerAddress = await signer.getAddress();
 
-        const pokerTokenContract = PokerToken__factory.connect(PokerToken.address, signer);
+        const aliceSigner = provider.getSigner(0);
+        const aliceAddress = await aliceSigner.getAddress();
 
-        await pokerTokenContract.mint(signerAddress, GameConstants.MIN_FUNDS);
-        let playerFunds = await pokerTokenContract.balanceOf(signerAddress);
-        await pokerTokenContract.approve(TurnBasedGameLobby.address, playerFunds);
+        const bobSigner = provider.getSigner(1);
+        const bobAddress = await bobSigner.getAddress();
+
+        const pokerTokenContractAlice = PokerToken__factory.connect(PokerToken.address, aliceSigner);
+        const pokerTokenContractBob = PokerToken__factory.connect(PokerToken.address, bobSigner);
+
+        // Mint tokens (Alice has minter role) for players
+        await pokerTokenContractAlice.mint(aliceAddress, GameConstants.MIN_FUNDS);
+        await pokerTokenContractAlice.mint(bobAddress, GameConstants.MIN_FUNDS);
+
+        // Setup for Alice
+        let aliceFunds = await pokerTokenContractAlice.balanceOf(aliceAddress);
+        await pokerTokenContractAlice.approve(TurnBasedGameLobby.address, aliceFunds);
+
+        // Setup for Bob
+        let bobFunds = await pokerTokenContractBob.balanceOf(bobAddress);
+        await pokerTokenContractBob.approve(TurnBasedGameLobby.address, bobFunds);
     });
 
-    it('should allow a player to join a game', async () => {
-        //const { name, avatar } = GameVars.gameData;
-        const playerInfo = { name: "Name", avatar: 1 };
+    it.skip('should allow a player to join a game', async () => {
+        const playerInfo = { name: "Alice", avatar: 1 };
 
         let gameReadyStatus: boolean = false;
         let gameReadyCallback = function () {
@@ -34,5 +46,26 @@ describe('LobbyWeb3', () => {
         await LobbyWeb3.joinGame(playerInfo, gameReadyCallback);
 
         expect(gameReadyStatus).to.be.false;
+    });
+
+    it('should notify game ready when the correct number of players have joined', async () => {
+        const player1Info = { name: "Alice", avatar: 1 };
+        const player2Info = { name: "Bob", avatar: 2 };
+
+        // Player 1 joins the game
+        let gameReadyStatusPlayer1: boolean = false;
+        let gameReadyCallbackPlayer1 = function () {
+            gameReadyStatusPlayer1 = true;
+        };
+        await LobbyWeb3.joinGame(player1Info, gameReadyCallbackPlayer1);
+        expect(gameReadyStatusPlayer1).to.be.false;
+
+        // Player 2 joing the game
+        let gameReadyStatusPlayer2: boolean = false;
+        let gameReadyCallbackPlayer2 = function () {
+            gameReadyStatusPlayer2 = true;
+        };
+        await LobbyWeb3.joinGame(player2Info, gameReadyCallbackPlayer2);
+        expect(gameReadyStatusPlayer2).to.be.true;
     });
 });
