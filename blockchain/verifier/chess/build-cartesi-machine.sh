aaf956c0071db6b43873a0378d9aad5cefcb312d
Milton Jonathan <milton.jonathan@cartesi.io>
2021-07-19 13:06:30 -0300

    Chess build-cartesi-machine script

---
+++ blockchain/verifier/chess/build-cartesi-machine.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# general definitions
+MACHINES_DIR=.
+MACHINE_TEMP_DIR=__temp_machine
+CARTESI_PLAYGROUND_DOCKER=cartesi/playground:0.3.0
+
+# set machines directory to specified path if provided
+if [ $1 ]; then
+  MACHINES_DIR=$1
+fi
+
+# removes machine temp store directory if it exists
+if [ -d "$MACHINE_TEMP_DIR" ]; then
+  rm -r $MACHINE_TEMP_DIR
+fi
+
+# builds machine (running with 0 cycles)
+# - initial (template) hash is printed on screen
+# - machine is stored in temporary directory
+docker run \
+  -e USER=$(id -u -n) \
+  -e GROUP=$(id -g -n) \
+  -e UID=$(id -u) \
+  -e GID=$(id -g) \
+  -v `pwd`:/home/$(id -u -n) \
+  -w /home/$(id -u -n) \
+  --rm $CARTESI_PLAYGROUND_DOCKER cartesi-machine \
+    --max-mcycle=0 \
+    --initial-hash \
+    --store="$MACHINE_TEMP_DIR" \
+    --flash-drive="label:verifier,filename:chess-verifier.ext2" \
+    --flash-drive="label:metadata,length:1<<12" \
+    --flash-drive="label:players,length:1<<12" \
+    --flash-drive="label:turnsMetadata,length:1<<16" \
+    --flash-drive="label:turnsData,length:1<<20" \
+    --flash-drive="label:verificationInfo,length:1<<12" \
+    --flash-drive="label:output,length:1<<12" \
+    -- $'/mnt/verifier/chess-run.sh'
+
+# defines target directory as being within $MACHINES_DIR and named after the stored machine's hash
+MACHINE_TARGET_DIR=$MACHINES_DIR/$(docker run \
+  -e USER=$(id -u -n) \
+  -e GROUP=$(id -g -n) \
+  -e UID=$(id -u) \
+  -e GID=$(id -g) \
+  -v `pwd`:/home/$(id -u -n) \
+  -h playground \
+  -w /home/$(id -u -n) \
+  --rm $CARTESI_PLAYGROUND_DOCKER cartesi-machine-stored-hash $MACHINE_TEMP_DIR/)
+
+# moves stored machine to the target directory
+if [ -d "$MACHINE_TARGET_DIR" ]; then
+  rm -r $MACHINE_TARGET_DIR
+fi
+mv $MACHINE_TEMP_DIR $MACHINE_TARGET_DIR
