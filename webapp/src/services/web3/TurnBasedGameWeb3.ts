ffaf6172c5070705e638ed214728b0b1c779ae06
Milton Jonathan <milton.jonathan@cartesi.io>
2021-10-09 07:56:44 -0300

    Initial impl for TurnBasedGameWeb3 challenge+verif

--- webapp/src/services/web3/TurnBasedGameWeb3.ts
+++ webapp/src/services/web3/TurnBasedGameWeb3.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import { BigNumber } from "ethers";
 import { TurnBasedGame } from "../TurnBasedGame";
 import TurnBasedGameJson from "../../abis/TurnBasedGame.json";
 import TurnBasedGameContextJson from "../../abis/TurnBasedGameContext.json";
@@ -8,6 +9,9 @@ import { TurnBasedGameContext__factory } from "../../types";
 import LoggerJson from "../../abis/Logger.json";
 import { Logger as LoggerContract } from "../../types";
 import { Logger__factory } from "../../types";
+import DescartesJson from "../../abis/Descartes.json";
+import { DescartesInterface as DescartesContract } from "../../types";
+import { DescartesInterface__factory as Descartes__factory } from "../../types";
 import { ServiceConfig } from "../ServiceConfig";
 import { ErrorHandler } from "../ErrorHandler";
 import { VerificationState } from "../Game";
@@ -20,19 +24,21 @@ import { VerificationState } from "../Game";
 export class TurnBasedGameWeb3 implements TurnBasedGame {
     gameIndex: number;
 
-    gameContract: TurnBasedGameContract;
+    descartesContract: DescartesContract;
     loggerContract: LoggerContract;
+    gameContract: TurnBasedGameContract;
     gameContextContract: any;
 
     turnDataQueue: Array<any>;
     onTurnOverReceivedResolvers: Array<(any) => any>;
 
     claimedResult: any;
-    onResultClaimReceived: (any) => any;
-    onGameOverReceived: (any) => any;
+    onResultClaimReceived: (claimedResult: any) => any;
+    onGameOverReceived: (confirmedResult: any) => any;
 
-    onGameChallengeReceived: (string) => any;
-    onVerificationUpdate: (VerificationState, string) => any;
+    descartesFinishedListener: (descartesIndexFinished: BigNumber, state: string) => void;
+    onGameChallengeReceived: (msg: string) => any;
+    onVerificationUpdate: (state: VerificationState, msg: string) => any;
 
     constructor(gameIndex: number) {
         this.gameIndex = gameIndex;
@@ -54,6 +60,7 @@ export class TurnBasedGameWeb3 implements TurnBasedGame {
         const signer = ServiceConfig.getSigner();
 
         // connects to smart contracts
+        this.descartesContract = Descartes__factory.connect(DescartesJson.address, signer);
         this.loggerContract = Logger__factory.connect(LoggerJson.address, signer);
         this.gameContract = TurnBasedGame__factory.connect(TurnBasedGameJson.address, signer);
         const contextContract = TurnBasedGameContext__factory.connect(TurnBasedGameContextJson.address, signer);
@@ -69,6 +76,10 @@ export class TurnBasedGameWeb3 implements TurnBasedGame {
         // sets up listener for GameResultClaimed event
         const gameResultClaimedFilter = this.gameContextContract.filters.GameResultClaimed(this.gameIndex, null, null);
         this.gameContextContract.on(gameResultClaimedFilter, this.onClaimResult.bind(this));
+
+        // sets up listener for GameChallenged event
+        const gameChallengedFilter = this.gameContextContract.filters.GameChallenged(this.gameIndex, null, null);
+        this.gameContextContract.on(gameChallengedFilter, this.onGameChallenged.bind(this));
 
         // sets up listener for GameEnd event
         const gameEndFilter = this.gameContextContract.filters.GameOver(this.gameIndex, null);
@@ -217,7 +228,32 @@ export class TurnBasedGameWeb3 implements TurnBasedGame {
     // CHALLENGE AND VERIFICATION
     //
     async challengeGame(msg: string): Promise<void> {
-        // TODO: call smart contract
+        await this.initWeb3();
+        await ErrorHandler.execute("challengeGame", async () => {
+            const tx = await this.gameContract.challengeGame(this.gameIndex);
+            console.log(`Challenged game '${this.gameIndex}' (tx: ${tx.hash} ; blocknumber: ${tx.blockNumber})`);
+        });
+    }
+    onGameChallenged(gameIndex, descartesIndex, author) {
+        // turns off previous listener for DescartesFinished events, if there was one
+        if (this.descartesFinishedListener) {
+            this.descartesContract.off("DescartesFinished", this.descartesFinishedListener);
+        }
+        // creates listener based on provided `descartesIndex`
+        this.descartesFinishedListener = function (descartesIndexFinished: BigNumber, state: string) {
+            if (descartesIndexFinished.eq(descartesIndex)) {
+                const stateStr = ethers.utils.toUtf8String(state);
+                if (stateStr === "ConsensusResult") {
+                    // result successfully computed: apply it
+                    this.applyVerificationResult();
+                } else {
+                    // error computing result: try again
+                    this.challengeGame(`Verification failed with state ${stateStr}`);
+                }
+            }
+        };
+        // sets up listener for DescartesFinished events
+        this.descartesContract.on("DescartesFinished", this.descartesFinishedListener);
     }
     receiveGameChallenged(): Promise<any> {
         return new Promise<any>((resolve) => {
@@ -230,6 +266,12 @@ export class TurnBasedGameWeb3 implements TurnBasedGame {
         });
     }
     async applyVerificationResult(): Promise<any> {
-        // TODO: call smart contract
+        await this.initWeb3();
+        await ErrorHandler.execute("applyVerificationResult", async () => {
+            const tx = await this.gameContract.applyVerificationResult(this.gameIndex);
+            console.log(
+                `Applying verification result for game '${this.gameIndex}' (tx: ${tx.hash} ; blocknumber: ${tx.blockNumber})`
+            );
+        });
     }
 }
