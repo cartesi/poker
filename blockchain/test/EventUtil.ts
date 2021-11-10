dac11370ab1334bca8949099ddfc70b6a5de4b41
Milton Jonathan <milton.jonathan@cartesi.io>
2021-11-10 06:17:50 -0300

    Improve EventUtil.getEvent() utility method - Better comments in appropriate format - Add typings

--- blockchain/test/EventUtil.ts
+++ blockchain/test/EventUtil.ts
@@ -1,20 +1,25 @@
-// Try to return an expected event in an given events array
-//
-// Parameters:
-// eventName expected event
-// parser contract that know the expected event definition
-// events array of events that will be evaluated
-export function getEvent(eventName, parser, events) {
-    let expectedEvent = null;
-    for (let i = 0; i < events.length; i++) {
-        let found = false;
+import { LogDescription } from "@ethersproject/abi";
+import { ethers } from "ethers";
+
+/**
+ * Attempts to retrieve the first instance of the specified event from a given event logs array
+ * @param eventName name/type of the event being retrieved
+ * @param parser contract instance used to parse the event logs
+ * @param eventLogs array of event logs (e.g., returned from a transaction)
+ * @returns the expected parsed event log, or undefined if no corresponding event was found
+ */
+export function getEvent(eventName: string, parser: ethers.Contract, eventLogs: Array<any>): LogDescription | undefined {
+    let expectedEvent: LogDescription;
+    for (let i = 0; i < eventLogs.length; i++) {
         try {
-            expectedEvent = parser.interface.parseLog(events[i]);
-            if (expectedEvent.name == eventName) found = true;
+            expectedEvent = parser.interface.parseLog(eventLogs[i]);
+            if (expectedEvent.name == eventName) {
+                return expectedEvent;
+            }
         } catch (e) {
-            // do nothing, just skip to the next try
+            // do nothing, just skip to try parsing the next event
         }
-        if (found) break;
     }
-    return expectedEvent;
+    // no corresponding event found
+    return undefined;
 }
