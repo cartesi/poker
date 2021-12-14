061885d68ee11d164d1388bb8a8f63694ea11f29
Milton Jonathan <milton.jonathan@cartesi.io>
2021-12-14 03:33:00 -0300

    Use official validator nodes for matic_testnet

--- webapp/src/GameConstants.ts
+++ webapp/src/GameConstants.ts
@@ -38,8 +38,7 @@ export class GameConstants {
     public static readonly GAME_METADATA = "0x";
     public static readonly VALIDATORS = {
         // Matic validators: known accounts used by the validator nodes
-        // TODO: setup real validator node accounts
-        "0x13881": ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"],
+        "0x13881": ["0x3a1eda296950Fa2bd18A9af5FeAdD50D993e5137", "0x882940b5B72c907eC7822eE59C8a11F40d30Cf08"],
         // Local validators: use default Hardhat accounts
         "0x7a69": ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"],
         "0x539": ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"],
