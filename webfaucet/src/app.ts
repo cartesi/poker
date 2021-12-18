3c8a7288b2e1bf32ab83f6e71e58b5ad7632484a
Milton Jonathan <milton.jonathan@cartesi.io>
2021-12-18 20:10:55 -0300

    Refactor webfaucet app to allow ETH/MATIC requests
    Using a hard-coded internal wallet to allow people to request tokens
    without having to connect any external wallet

--- webfaucet/src/app.ts
+++ webfaucet/src/app.ts
@@ -1,107 +1,48 @@
 import { ethers } from "ethers";
-import Portis from "@portis/web3";
 import PokerToken from "./abis/PokerToken.json";
 import PokerTokenFaucet from "./abis/PokerTokenFaucet.json";
 
-declare let window: any;
-
-enum Wallet {
-    METAMASK = "METAMASK",
-    PORTIS = "PORTIS",
-}
-
-const PORTIS_APP_ID = "15ce62b0-b226-4e6f-9f8d-abbdf8f2cda2";
-
-const DEFAULT_CHAIN_URL = "https://matic-testnet-archive-rpc.bwarelabs.com";
-const DEFAULT_CHAIN_ID = "0x13881";
+const CHAIN_URL = "https://matic-testnet-archive-rpc.bwarelabs.com";
+const j = `{"address":"0a0ee891887c2785beb86f186f44e8b5e22ea7a9","id":"f6ea2099-28d1-4762-a595-ec7a6612517c","version":3,"Crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"8f7d09f10c9d163fe044ef4d039bfdb9"},"ciphertext":"28922d7465eb1adafd7c7881937c43ac9073068fb024b0a18e125c068a16b890","kdf":"scrypt","kdfparams":{"salt":"5e70e29acb906ccd928d5e46b1f6096cc33627138c26c1ebd9cc61c7d1cfc4a9","n":131072,"dklen":32,"p":1,"r":8},"mac":"1087bdb4cc14a36b46052ad7aa39a9c72f3e61dfff856599669d4ec66b3768d4"},"x-ethers":{"client":"ethers.js","gethFilename":"UTC--2021-12-18T22-08-45.0Z--0a0ee891887c2785beb86f186f44e8b5e22ea7a9","mnemonicCounter":"c8ff75074bda324af11ebd3da3e59284","mnemonicCiphertext":"4d1ed397abc7bf450663102f8cc43564","path":"m/44'/60'/0'/0/0","locale":"en","version":"0.1"}}`;
+const p = "xPJG8EC$6Rr3";
 
 class PokerFaucet {
-    private selectedWallet = Wallet.METAMASK;
-    private provider;
-
-    public static readonly CHAINS = {
-        "0x13881": "Matic Testnet",
-        "0x7a69": "Local Network",
-        "0x539": "Local Network",
-    };
+    private signer;
 
     async init() {
-        this.initGUI();
-        this.setWalletRadioListener();
+        this.showLoadingInfo();
         this.setTargetInputListener();
         this.setRequestButtonListener();
 
-        this.provider = await this.getProvider();
+        this.signer = await this.getSigner();
         await this.updateGUI();
-    }
-
-    initGUI() {
-        // Check default wallet
-        let radios = document.getElementsByName("wallet");
-        for (let i = 0; i < radios.length; i++) {
-            if (radios[i].id == Wallet.METAMASK) {
-                radios[i].setAttribute("checked", "true");
-                break;
-            }
-        }
-
-        // Loading info (Portis takes some time to load)
-        this.showLoadingInfo();
     }
 
     showLoadingInfo() {
         (document.getElementById("requestButton") as HTMLButtonElement).disabled = true;
-
-        document.getElementById("network").innerHTML = "Loading...";
-        document.getElementById("address").innerHTML = "Loading...";
-        document.getElementById("balance").innerHTML = "Loading...";
+        document.getElementById("balanceMatic").innerHTML = "Loading...";
+        document.getElementById("balancePoker").innerHTML = "Loading...";
     }
 
     async updateGUI() {
-        const signer = this.provider.getSigner();
-        const address = await signer.getAddress();
-        const chainId = await this.getChainId();
-        const network = PokerFaucet.CHAINS[chainId] ? PokerFaucet.CHAINS[chainId] : "Unsupported Network";
-
-        const tokenContract = new ethers.Contract(PokerToken.address, PokerToken.abi, signer);
-        document.getElementById("network").innerHTML = network;
-        document.getElementById("address").innerHTML = address;
-
+        const tokenContract = new ethers.Contract(PokerToken.address, PokerToken.abi, this.signer);
         const targetInput = <HTMLInputElement>document.getElementById("target");
         const requestButton = document.getElementById("requestButton") as HTMLButtonElement;
         try {
             if (targetInput.value) {
-                document.getElementById("balance").innerHTML = await tokenContract.balanceOf(targetInput.value);
+                document.getElementById("balanceMatic").innerHTML = ethers.utils.formatEther(
+                    await this.signer.provider.getBalance(targetInput.value)
+                );
+                document.getElementById("balancePoker").innerHTML = await tokenContract.balanceOf(targetInput.value);
                 requestButton.disabled = false;
                 return;
             }
         } catch (error) {
             // normal, input is not a valid address
         }
-        document.getElementById("balance").innerHTML = "N/A";
+        document.getElementById("balanceMatic").innerHTML = "N/A";
+        document.getElementById("balancePoker").innerHTML = "N/A";
         requestButton.disabled = true;
-    }
-
-    setWalletRadioListener() {
-        let radios = document.getElementsByName("wallet");
-        for (let i = 0; i < radios.length; i++) {
-            radios[i].addEventListener("change", async (e) => {
-                let id = (e.target as HTMLElement).id;
-                switch (id) {
-                    case Wallet.PORTIS:
-                        this.selectedWallet = Wallet.PORTIS;
-                        break;
-                    case Wallet.METAMASK:
-                        this.selectedWallet = Wallet.METAMASK;
-                        break;
-                    default:
-                        throw new Error("Unsupported wallet was found");
-                }
-                this.showLoadingInfo();
-                this.provider = await this.getProvider();
-                await this.updateGUI();
-            });
-        }
     }
 
     setTargetInputListener() {
@@ -114,93 +55,66 @@ class PokerFaucet {
         button.addEventListener("click", this.request.bind(this));
     }
 
-    async getChainId(): Promise<string> {
-        switch (this.selectedWallet) {
-            case Wallet.METAMASK:
-                return window.ethereum.chainId;
-            default:
-                return DEFAULT_CHAIN_ID;
+    async getSigner(): Promise<any> {
+        if (!this.signer) {
+            const provider = new ethers.providers.JsonRpcProvider(CHAIN_URL);
+            const wallet = await ethers.Wallet.fromEncryptedJson(j, p);
+            this.signer = wallet.connect(provider);
+            console.log(`Configured signer with address ${wallet.address}`);
         }
-    }
-
-    async getProvider(): Promise<any> {
-        switch (this.selectedWallet) {
-            case Wallet.PORTIS:
-                this.provider = this.getProviderFromPortis();
-                break;
-            case Wallet.METAMASK:
-                this.provider = this.getProviderFromMetamask();
-                break;
-            default:
-                throw new Error("Unsupported wallet was found");
-        }
-        if (this.provider) {
-            return this.provider;
-        } else {
-            throw new Error("Unable to instantiate the provider to connect to the network");
-        }
-    }
-
-    async getProviderFromPortis(): Promise<any> {
-        console.log("Using Portis as provider");
-
-        let portis;
-        try {
-            portis = new Portis(PORTIS_APP_ID, {
-                nodeUrl: DEFAULT_CHAIN_URL,
-                chainId: DEFAULT_CHAIN_ID,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-
-        portis.showPortis();
-
-        return new Promise((resolve) => {
-            portis.isLoggedIn().then(({ error, result }) => {
-                let provider = new ethers.providers.Web3Provider(portis.provider);
-                resolve(provider);
-            });
-        });
-    }
-
-    async getProviderFromMetamask() {
-        console.log("Using Metamask as provider");
-
-        if (!window.ethereum) {
-            console.error("Cannot connect to window.ethereum. Is Metamask or a similar plugin installed?");
-            return;
-        }
-        // sets up hooks to update web3 when accounts or chain change
-        window.ethereum.on("accountsChanged", async () => {
-            this.showLoadingInfo();
-            await this.updateGUI();
-        });
-        window.ethereum.on("chainChanged", async () => {
-            this.showLoadingInfo();
-            this.provider = new ethers.providers.Web3Provider(window.ethereum);
-            await this.updateGUI();
-        });
-
-        // connects to ethereum account
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-
-        return new ethers.providers.Web3Provider(window.ethereum);
+        return this.signer;
     }
 
     async request() {
-        const signer = this.provider.getSigner();
         const address = (<HTMLInputElement>document.getElementById("target")).value;
 
-        const faucetContract = new ethers.Contract(PokerTokenFaucet.address, PokerTokenFaucet.abi, signer);
-
-        await faucetContract.requestTokens(address);
-
-        const amount = await faucetContract.TOKEN_AMOUNT();
-        alert(`Requested ${amount} POKER tokens for ${address}`);
+        const faucetContract = new ethers.Contract(PokerTokenFaucet.address, PokerTokenFaucet.abi, this.signer);
 
         this.showLoadingInfo();
+
+        // request MATIC tokens
+        let amountMatic;
+        let txMatic;
+        try {
+            amountMatic = ethers.utils.formatEther(await faucetContract.getRequestFundsAmount());
+            txMatic = await faucetContract.requestFunds(address);
+        } catch (error) {
+            console.error(`Failed to request MATIC: ${JSON.stringify(error)}`);
+        }
+
+        // request POKER tokens
+        let amountPoker;
+        let txPoker;
+        try {
+            amountPoker = await faucetContract.getRequestTokensAmount();
+            txPoker = await faucetContract.requestTokens(address);
+        } catch (error) {
+            console.error(`Failed to request POKER: ${JSON.stringify(error)}`);
+        }
+
+        // inform user of request results
+        let msg;
+        if (txMatic && txPoker) {
+            msg = `Successfully requested ${amountPoker} POKER tokens and ${amountMatic} MATIC for ${address}`;
+        } else if (txMatic) {
+            msg = `Successfully requested ${amountMatic} MATIC for ${address} (failed to request POKER tokens)`;
+        } else if (txPoker) {
+            msg = `Successfully requested ${amountPoker} POKER tokens for ${address} (failed to request MATIC)`;
+        } else {
+            msg = `Failed to request POKER and MATIC for ${address}`;
+        }
+
+        document.getElementById("message").innerHTML = msg;
+
+        if (txMatic) {
+            await txMatic.wait();
+        }
+        if (txPoker) {
+            await txPoker.wait();
+        }
+
         await this.updateGUI();
+        document.getElementById("message").innerHTML = "";
     }
 }
 
