21dc6ce6498c09963439d637d45f32acccb20e8d
Marcel Moura <marcel.moura@cartesi.io>
2021-12-21 21:24:37 -0300

    Disable HW acceleration and rasterizer
    This improves rendering on Linux

--- electron-app/electron_src/index.ts
+++ electron-app/electron_src/index.ts
@@ -1,6 +1,9 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch("disable-software-rasterizer");
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
     // eslint-disable-line global-require
